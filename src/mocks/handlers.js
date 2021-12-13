1af4fd83d1d8a1e05089ce057fd9ef0f0bb7ec5a
Steven Hill <steven@stevenhill.co.za>
2021-12-13 20:19:26 +0200

    Mock Service Worker: response resolver

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,10 @@
 import { rest } from 'msw';
 
-const handlers = [rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-
-return res([
-    {name: 'chocolate', imagePath: '/images/chocolate.png' }
-    {name: 'vanilla', imagePath: '/images/vanilla.png' }
-])
-
-})];
+const handlers = [
+  rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+    return res([
+      { name: 'chocolate', imagePath: '/images/chocolate.png' },
+      { name: 'vanilla', imagePath: '/images/vanilla.png' },
+    ]);
+  }),
+];
