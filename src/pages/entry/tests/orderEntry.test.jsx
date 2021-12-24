a570e2e176dceace78ca3490b536f7c7159ff064
Steven Hill <steven@stevenhill.co.za>
2021-12-24 06:59:54 +0200

    did not return MSW@ solved

--- src/pages/entry/tests/orderEntry.test.jsx
+++ src/pages/entry/tests/orderEntry.test.jsx
@@ -6,10 +6,10 @@ import { render, screen, waitFor } from '@testing-library/react';
 test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-      res(ctx.status(500));
+      return res(ctx.status(500));
     }),
     rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
-      res(ctx.status(500));
+      return res(ctx.status(500));
     }),
   );
 
