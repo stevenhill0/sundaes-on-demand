b4b9a1661c39874db3c6235904bc11a21099eaa7
Steven Hill <steven@stevenhill.co.za>
2021-12-25 15:49:57 +0200

    component provider wrapper

---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+
+test('Update scoop subtotal when scoops change', async () => {
+  render(<Options optionType="scoops" />);
+
+  // exact option is by default true. If it is only a partial match use exact with a false value
+  const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false }); // if it is a display element you can just use getByText insread of Role
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1'); //how many scoops
+  expect(scoopsSubtotal).toHaveTextContent('2.00'); //Dollar value
+
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, '2');
+  expect(scoopsSubtotal).toHaveTextContent('6:00');
+});
